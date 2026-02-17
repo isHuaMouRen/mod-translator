766919b4396330e97ac31a280b6b940c8c8b3950
HuaMouRen <2825338723@qq.com>
2026-02-17 11:03:20 +0800

    feat(utils): 添加了两个工具
    1. DialogManager
    2. ErrorReportDialog

---
+++ MinecraftModTranslator/Utils/DialogManager.cs
@@ -0,0 +1,41 @@
+﻿namespace MinecraftModTranslator.Utils
+{
+    using ModernWpf.Controls;
+
+    public static class DialogManager
+    {
+        private static readonly SemaphoreSlim DialogSemaphore = new(1, 1);
+
+        public static async Task<ContentDialogResult> ShowDialogAsync(
+            ContentDialog dialog,
+            Action? primaryCallback = null,
+            Action? secondaryCallback = null,
+            Action? closeCallback = null)
+        {
+
+            await DialogSemaphore.WaitAsync();
+            try
+            {
+                var result = await dialog.ShowAsync();
+
+                // 执行回调
+                switch (result)
+                {
+                    case ContentDialogResult.Primary: primaryCallback?.Invoke(); break;
+                    case ContentDialogResult.Secondary: secondaryCallback?.Invoke(); break;
+                    default: closeCallback?.Invoke(); break;
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return ContentDialogResult.None;
+            }
+            finally
+            {
+                DialogSemaphore.Release();
+            }
+        }
+    }
+}
---
+++ MinecraftModTranslator/Utils/ErrorReportDialog.cs
@@ -0,0 +1,68 @@
+﻿namespace MinecraftModTranslator.Utils
+{
+    using Ookii.Dialogs.Wpf;
+    using System.Windows;
+
+    public static class ErrorReportDialog
+    {
+        public static async void Show(Exception ex, bool isUnHandleException = false)
+        {
+            var dialog = new TaskDialog
+            {
+                WindowTitle = isUnHandleException ? "发生未捕获的错误" : "发生错误",
+                MainIcon = TaskDialogIcon.Error,
+                MainInstruction = isUnHandleException ? "程序在运行时发生了未捕获的错误" : "程序在运行时发生错误",
+                Content = ex.ToString(),
+                AllowDialogCancellation = false,
+                ButtonStyle = TaskDialogButtonStyle.CommandLinks
+            };
+
+
+            var btnEnd = new TaskDialogButton
+            {
+                Text = "终止程序（推荐）",
+                CommandLinkNote = "丢弃现在状态并退出程序"
+            };
+            var btnCopyAndEnd = new TaskDialogButton
+            {
+                Text = "复制错误信息并退出",
+                CommandLinkNote = "复制错误信息并退出程序"
+            };
+            var btnContinue = new TaskDialogButton
+            {
+                Text = "继续运行（不推荐）",
+                CommandLinkNote = "忽略这个错误并继续执行程序（出现错误后程序一般无法正常运行）"
+            };
+
+            var btnClose = new TaskDialogButton
+            {
+                Text = "关闭",
+                ButtonType = ButtonType.Close
+            };
+
+            dialog.Buttons.Add(btnEnd);
+            dialog.Buttons.Add(btnCopyAndEnd);
+
+            if (!isUnHandleException)
+            {
+                dialog.Buttons.Add(btnContinue);
+            }
+
+            dialog.Buttons.Add(btnClose);
+
+
+            var result = dialog.ShowDialog();
+
+
+            if (result == btnEnd)
+            {
+                Environment.Exit(1);
+            }
+            else if (result == btnCopyAndEnd)
+            {
+                Clipboard.SetText($"{new string('=', 5)}[错误信息]{new string('=', 5)}\n\n{ex}\n\n{new string('=', 25)}");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
