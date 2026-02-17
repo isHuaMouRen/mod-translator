3d2e208caff1c96dab9fb0dc084ce1d5661f16ed
HuaMouRen <2825338723@qq.com>
2026-02-17 18:04:37 +0800

    imp(globals): 变更版本号

--- MinecraftModTranslator/Classes/Globals.cs
+++ MinecraftModTranslator/Classes/Globals.cs
@@ -7,7 +7,7 @@ namespace MinecraftModTranslator.Classes
 {
     public static class Globals
     {
-        public static string Version = "1.0.0-beta.1";//版本
+        public static string Version = "1.0.0";//版本
 
 
         public static string? ModJarPath;//Jar路径
