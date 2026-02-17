b681037de6fb41016c2bd7dfb80e16a4a2d90b1d
HuaMouRen <2825338723@qq.com>
2026-02-17 13:06:39 +0800

    imp(json-fabric-info): 属性为可控

--- MinecraftModTranslator/Classes/JsonConfigs/JsonFabricInfo.cs
+++ MinecraftModTranslator/Classes/JsonConfigs/JsonFabricInfo.cs
@@ -7,13 +7,13 @@ namespace MinecraftModTranslator.Classes.JsonConfigs
         public class Index
         {
             [JsonProperty("name")]
-            public string Name { get; set; } = "Name";
+            public string? Name { get; set; }
 
             [JsonProperty("description")]
-            public string Description { get; set; } = "Description";
+            public string? Description { get; set; }
 
             [JsonProperty("icon")]
-            public string Icon { get; set; } = "icon.png";
+            public string? Icon { get; set; }
         }
     }
 }
